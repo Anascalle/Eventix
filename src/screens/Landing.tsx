586a6f321f143fa7a18ef99cec582e76205f19cc
Anascalle <127751671+Anascalle@users.noreply.github.com>
2024-09-27 16:36:23 -0500

    new scaffolding

---
+++ src/screens/Landing.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Nav1 from '../components/NavBarLanding/NavBarLanding.View';
+import Banner from '../components/Banner/Banner.View';
+import SectionGroup from '../components/SectionGroup/SectionGroup.View';
+import Footer from '../components/Footer/Footer.View';
+
+
+const LandingScreen: React.FC = () => {
+  return (
+    <div className="landing-screen">
+         <Nav1></Nav1>
+         <Banner></Banner>
+         <SectionGroup></SectionGroup>
+         <Footer></Footer>
+      </div>
+   
+  );
+};
+
+export default LandingScreen;
