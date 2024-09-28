6bc48ced2e031e512e9b0df09e3211fa6cf964bc
valen-web <128190072+valen-web@users.noreply.github.com>
2024-09-28 01:53:50 -0500

    Invitations_cards

--- src/components/Main/Invitations/invitations.tsx
+++ src/components/Main/Invitations/invitations.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import "./invitations.css"
+import "./invitations.css";
+import porfiles from "../data/data";
+import InvitationsCards from "./cards";
 
-interface InvitationsProps {
-    name: string;
-    ocation: string;
-    date: string; 
-    url: string;
-}
-
-const Invitations: React.FC<InvitationsProps> = ({ name, ocation, date, url }) => {
-    return (
-        <div id="invitations_div">
-            <h2>Invitations</h2>
-            <div id="Invitation">
-                <img src={url} alt="" />
-                <p id="name">{name} sent you an invitation</p>
-                <p id="ocation">Ocation: <span id="ocation_type">{ocation} Grade</span></p>
-                <p id="date">Date: <span id="date_number">{date}01/10/2024</span></p>
-            </div>
-        </div>
-    );
-}
+const Invitations: React.FC = (
+) => {
+  return (
+    <div id="invitations_shadow">
+      <div id="invitations_div">
+        <h2>Invitations</h2>
+        {porfiles.map((profile, index) => (
+        <InvitationsCards
+          key={index}
+          name={profile.name}
+          ocation={profile.Ocation}
+          date={profile.Date}
+          url={profile.img}
+        />
+      ))}
+      </div>
+    </div>
+  );
+};
 
 export default Invitations;
