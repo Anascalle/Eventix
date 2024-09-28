4c0ef224b3a719e5e1482191d50280440e800469
valen-web <128190072+valen-web@users.noreply.github.com>
2024-09-28 01:55:51 -0500

    data

---
+++ src/components/Main/data/data.tsx
@@ -0,0 +1,25 @@
+const porfiles = [{
+    name : "Ana",
+    Ocation: "Grade",
+    Date: "01/10/2024", 
+    img: "https://raw.githubusercontent.com/valen-web/LAB3/refs/heads/main/Porfileicon2.png"
+},{
+    name : "Sebas",
+    Ocation: "Aniversary",
+    Date: "01/10/2024",
+    img: "https://raw.githubusercontent.com/valen-web/LAB3/refs/heads/main/porfileicon3.png"
+},{
+    name : "David",
+    Ocation: "Wedding",
+    Date: "01/10/2024",
+    img: "https://raw.githubusercontent.com/valen-web/LAB3/refs/heads/main/porfileicon3.png"
+},{
+    name : "Valen",
+    Ocation: "Birthday",
+    Date: "01/10/2024",
+    img: "https://raw.githubusercontent.com/valen-web/LAB3/refs/heads/main/Porfileicon2.png"
+},
+]
+
+export default porfiles
+    
