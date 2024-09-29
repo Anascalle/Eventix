5203d7d3c3ca629ef052abda743d7d2f4ae929e7
valen-web <128190072+valen-web@users.noreply.github.com>
2024-09-28 23:20:24 -0500

    Add files via upload

--- screens/Main/Main.tsx
+++ screens/Main/Main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'; 
 import Nav2 from '../../components/Main/Nav/Nav';
-
 import "./Main.css"
 import Invitations from '../../components/Main/Invitations/invitations';
+import Host from '../../components/Main/Host/cards';
+import Guest from '../../components/Main/Guest/cards';
+import NewEventButton from '../../components/Main/NewEventButton/NewEventButton';
 
 const Main: React.FC = () => {
     return (
      <div id='main_screen'>
        <Nav2></Nav2>
         <Invitations></Invitations>
+        <Host></Host>
+        <Guest></Guest>
+        <NewEventButton></NewEventButton>
     </div>
     );
 };
