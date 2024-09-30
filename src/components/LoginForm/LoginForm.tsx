307ed03c7f45d8c93e4661fa8ddd3dd89552d96e
FuzzyDuck1930 <141763142+FuzzyDuck1930@users.noreply.github.com>
2024-09-30 02:26:41 -0500

    Auth working on SignUp

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,29 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from "../../service/firebaseConfig";
 import Inputs from "../Input/Inputs";
 import RegisterMessage from "../RegisterMessage/RegisterMessage";
 import RegisterButton from "../RegisterButton/RegisterButton";
 
 function LoginForm(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log('Usuario autenticado:', userCredential.user);
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error);
+        }
+        };
+
     return <form action="" id="loginForm">
     <RegisterMessage title="Log In" infoMessagept1="If you haven't registered yet" infoMessagept2="You can" infoLink="Register here !"></RegisterMessage>
-    <Inputs uid="loginEmail" lillogo="" info="Email" infoPlaceholder="Enter your email address"></Inputs>
-    <Inputs uid="loginPassword" lillogo="" info="Password" infoPlaceholder="Enter your password"></Inputs>
+    <Inputs uid="loginEmail" lillogo="" info="Email" infoPlaceholder="Enter your email address" onChange={(e) => setEmail(e.target.value)}></Inputs>
+    <Inputs uid="loginPassword" lillogo="" info="Password" infoPlaceholder="Enter your password" onChange={(e) => setPassword(e.target.value)}></Inputs>
     <RegisterButton btnId="" buttonText="Login"></RegisterButton>
     </form>
 }
