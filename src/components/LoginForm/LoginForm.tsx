13a8061ebfb4461213b0d164cb2c717b1ceaacb0
FuzzyDuck1930 <141763142+FuzzyDuck1930@users.noreply.github.com>
2024-09-30 02:29:02 -0500

    Auth working in Login

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -20,7 +20,7 @@ function LoginForm(){
         }
         };
 
-    return <form action="" id="loginForm">
+    return <form onSubmit={handleLogin} id="loginForm">
     <RegisterMessage title="Log In" infoMessagept1="If you haven't registered yet" infoMessagept2="You can" infoLink="Register here !"></RegisterMessage>
     <Inputs uid="loginEmail" lillogo="" info="Email" infoPlaceholder="Enter your email address" onChange={(e) => setEmail(e.target.value)}></Inputs>
     <Inputs uid="loginPassword" lillogo="" info="Password" infoPlaceholder="Enter your password" onChange={(e) => setPassword(e.target.value)}></Inputs>
