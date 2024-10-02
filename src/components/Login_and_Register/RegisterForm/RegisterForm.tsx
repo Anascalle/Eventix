308b80ed00822f70852b39737c29d47ab1817cca
FuzzyDuck1930 <141763142+FuzzyDuck1930@users.noreply.github.com>
2024-10-02 14:08:12 -0500

    Style_css added

--- src/components/Login_and_Register/RegisterForm/RegisterForm.tsx
+++ src/components/Login_and_Register/RegisterForm/RegisterForm.tsx
@@ -6,6 +6,7 @@ import Inputs from '../Input/Input';
 import RegisterMessage from "../RegisterMessage/RegisterMessage";
 import RegisterButton from "../RegisterButton/RegisterButton";
 import { useNavigate } from 'react-router-dom';
+import "./RegisterForm.css"
 
 const RegisterForm: React.FC = () => {
     const [email, setEmail] = useState('');
@@ -40,46 +41,51 @@ const RegisterForm: React.FC = () => {
     };
 
     return (
+        <div id='SignSpace'>
+            <img id='SignImg' src="https://firebasestorage.googleapis.com/v0/b/programacion-ec39e.appspot.com/o/medium-shot-friends-posing-photo-booth.jpg?alt=media&token=3d2c554e-6ea5-40a7-94cf-ec87f9e9fd35" alt="" />
+            <div className='registerSpace'>
         <form id="registerForm" onSubmit={handleRegister}>
-            <RegisterMessage 
-                title="Sign Up" 
-                infoMessagept1="If you already have an account registered" 
+            <RegisterMessage
+                title="Sign Up"
+                infoMessagept1="If you already have an account registered"
                 infoMessagept2="You can"
             />
-            <Inputs 
-                inputType="email" 
-                uid="registerEmail" 
-                lillogo="" 
-                info="Email" 
-                infoPlaceholder="Enter your email address" 
-                onChange={(e) => setEmail(e.target.value)} 
+            <Inputs
+                inputType="email"
+                uid="registerEmail"
+                lillogo="/mail.svg"
+                info="Email"
+                infoPlaceholder="Enter your email address"
+                onChange={(e) => setEmail(e.target.value)}
             />
-            <Inputs 
-                inputType="text" 
-                uid="registerUsername" 
-                lillogo="" 
-                info="Username" 
-                infoPlaceholder="Enter your username" 
-                onChange={(e) => setUsername(e.target.value)} 
+            <Inputs
+                inputType="text"
+                uid="registerUsername"
+                lillogo="/user.svg"
+                info="Username"
+                infoPlaceholder="Enter your username"
+                onChange={(e) => setUsername(e.target.value)}
             />
-            <Inputs 
-                inputType="password" 
-                uid="registerPassword" 
-                lillogo="" 
-                info="Password" 
-                infoPlaceholder="Enter your password" 
-                onChange={(e) => setPassword(e.target.value)} 
+            <Inputs
+                inputType="password"
+                uid="registerPassword"
+                lillogo="/lock.svg"
+                info="Password"
+                infoPlaceholder="Enter your password"
+                onChange={(e) => setPassword(e.target.value)}
             />
-            <Inputs 
-                inputType="number" 
-                uid="registerAccount" 
-                lillogo="" 
-                info="Account Amount" 
-                infoPlaceholder="Add account amount" 
-                onChange={(e) => setAccountAmount(e.target.value)} 
+            <Inputs
+                inputType="number"
+                uid="registerAccount"
+                lillogo="money.svg"
+                info="Account Amount"
+                infoPlaceholder="Add account amount"
+                onChange={(e) => setAccountAmount(e.target.value)}
             />
             <RegisterButton btnId="RegisterBtn" buttonText="Register" />
         </form>
+        </div>
+        </div>
     );
 };
 
--- src/components/Login_and_Register/RegisterMessage/RegisterMessage.tsx
+++ src/components/Login_and_Register/RegisterMessage/RegisterMessage.tsx
@@ -1,18 +1,19 @@
 import RegisterFormButton from "./RegisterMessageButton";
+import "./RegisterMessage.css"
 
 interface MessageProps {
     title: string;
     infoMessagept1: string;
     infoMessagept2: string;
-    
+
 }
 
 function RegisterMessage( { title, infoMessagept1, infoMessagept2,  }:MessageProps ){
-    return <div>
+    return <div id="MessageSpace">
         <h1>{title}</h1>
         <div>
         <p>{infoMessagept1}</p>
-            <div>
+            <div id="infoWithLink">
                 <p>{infoMessagept2}</p>
                 <RegisterFormButton/>
             </div>
