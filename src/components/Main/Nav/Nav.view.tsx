49b335c76bc4871f9787bf47eb7eeba1902e8daa
Anascalle <127751671+Anascalle@users.noreply.github.com>
2024-11-04 17:22:57 -0500

    Fix- nav_ main_correction

--- src/components/Main/Nav/Nav.view.tsx
+++ src/components/Main/Nav/Nav.view.tsx
@@ -10,47 +10,50 @@ const Nav2: React.FC = () => {
   const { userId } = useParams<{ userId: string }>();
   const scrolled = useScroll(); 
   const [userName, setUserName] = useState<string | null>(null);
+  const [userImg, setUserImg] = useState<string | null>(null); // Estado para almacenar la URL de la imagen
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        if (!userId) {
-          console.error("User ID is undefined.");
-          return; // Salir si el userId no está definido
-        }
-
+      if (user && userId) {
         try {
-          const userDocRef = doc(db, "users", userId); 
+          const userDocRef = doc(db, "users", userId);
           const userDoc = await getDoc(userDocRef);
 
           if (userDoc.exists()) {
             const userData = userDoc.data();
             setUserName(userData.username || null);
+            setUserImg(userData.img || null); // Obtener la URL de la imagen desde los datos del usuario
           } else {
             console.error("No such document!");
             setUserName(null);
+            setUserImg(null);
           }
         } catch (error) {
           console.error("Error fetching user document:", error);
           setUserName(null);
+          setUserImg(null);
         }
       } else {
-        setUserName(null); 
+        setUserName(null);
+        setUserImg(null);
       }
     });
 
-    return () => unsubscribe(); 
+    return () => unsubscribe();
   }, [userId]);
 
   return (
     <nav className={`nav_bar2 ${scrolled ? 'scrolled' : ''}`}>
       <h1 className="app_name">Eventix</h1>
       <ul className="nav_links">
-        
-        <li><a aria-label="About us" href="/">ABOUT US</a> </li>
-        <li><a aria-label="About" href="/about">SUPPORT</a></li>
+        <li><a aria-label="About us" href="/">ABOUT US</a></li>
+        <li><a aria-label="Support" href="/about">SUPPORT</a></li>
       </ul>
-      <img id="profile_img_nav" src="https://firebasestorage.googleapis.com/v0/b/programacion-ec39e.appspot.com/o/Personajes%2FPersonaje1.svg?alt=media&token=8c75c21d-de7c-430c-9246-fdadb91db2d7" alt="Profile" />
+      <img
+        id="profile_img_nav"
+        src={userImg || "https://via.placeholder.com/50"} // Mostrar imagen de perfil o imagen de placeholder
+        alt="Profile"
+      />
       <p id="user_name_nav">{userName || "Guest"}</p>
     </nav>
   );
