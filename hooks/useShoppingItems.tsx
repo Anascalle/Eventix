6b4a97da7586903c62fb75686a7b5ee1ed4abcdb
Thescientifhic <127751129+Thescientifhic@users.noreply.github.com>
2024-11-06 23:21:23 -0500

    Function_promiseAll

---
+++ hooks/useShoppingItems.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { getItems } from "../utils/firebaseConfig";
+
+interface Item {
+    image: string;
+    name: string;
+    price: number;
+    disponibility: number;
+    eventType: string;
+}
+
+const useShoppingItems = () => {
+    const { state } = useLocation();
+    const initialEventType = state?.eventType || "";
+    const [items, setItems] = useState<Item[]>([]);
+    const [filterType, setFilterType] = useState<string>(initialEventType);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const itemsList = await getItems();
+            setItems(itemsList);
+        };
+
+        fetchItems();
+    }, []);
+
+    const filteredItems = filterType === "Other"
+        ? items 
+        : items.filter((item) => item.eventType === filterType);
+
+    return { filteredItems, filterType, setFilterType };
+};
+
+export default useShoppingItems;
