0dad6e0089862b9c8f9e1063f7472fa923301151
valen-web <128190072+valen-web@users.noreply.github.com>
2024-11-27 22:10:40 -0500

    detail event new css

--- src/components/DetailEvent/InfoCard/InfoCard.view.tsx
+++ src/components/DetailEvent/InfoCard/InfoCard.view.tsx
@@ -1,68 +1,161 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import ModalInvite from "../InviteDetailModal/InviteDetailModal";
+import BackBtn from "../BackBtn/BackBtn.view";
+import "./InfoCard.css";
 import FunctionBtn from "../FunctionBtns/FunctionBtns.view";
-import "./InfoCard.css"
+import useInviteLogic from "../../../hooks/useInviteLogic";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../../utils/firebaseConfig";
 
 interface InfoProp {
-    img: string;
-    NameEvent: string;
-    Host: string;
-    EventType: string;
-    DressCode: string;
-    Date: string;
-    StartTime: string;
-    Location: string;
-    Description: string;
+  img: string;
+  NameEvent: string;
+  Host: string;
+  EventType: string;
+  DressCode: string;
+  Date: string;
+  StartTime: string;
+  Location: string;
+  Description: string;
+}
+
+interface EventInfo {
+  name: string;
+  image: string;
+  host: string;
+  eventType: string;
+  dressCode: string;
+  date: string;
+  startTime: string;
+  location: string;
+  description: string;
 }
 
 const InfoCard: React.FC<InfoProp> = (prop) => {
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const eventId = state?.id;
 
-    const handleNextShooping = () => {
-        console.log("Shooping clicked");
+  const {
+    isModalOpen,
+    setIsModalOpen,
+    searchValue,
+    setSearchValue,
+    filteredUsers,
+    selectedUsers,
+    setSelectedUsers,
+    handleInviteUser,
+    handleNextInvite,
+  } = useInviteLogic(eventId);
+
+  const handleNextShooping = () => {
+    navigate(`/shopping/${eventId}`, {
+      state: { eventType: prop.EventType, eventId },
+    });
+  };
+
+  const handleNextFound = () => {
+    console.log("Found clicked");
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSearchValue("");
+    setSelectedUsers([]);
+  };
+
+  const [eventInfo, setEventInfo] = useState<EventInfo | null>(null);
+
+  useEffect(() => {
+    const fetchEventDetails = async () => {
+      const docRef = doc(db, "events", eventId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Event data:", docSnap.data());
+        setEventInfo(docSnap.data() as EventInfo);
+      } else {
+        console.log("No such document!");
+      }
     };
 
-    const handleNextFound = () => {
-        console.log("Found clicked");
-    };
+    fetchEventDetails();
+  }, [eventId]);
 
-    const handleNextInvite = () => {
-        console.log("Invite clicked");
-    };
-
-    return (
-        <div className="InfoCard">
-            <div className="ImgBanner">
-                <img src={prop.img} alt="" />
-            </div>
-            <div className="NameEvent">
-                <h1>{prop.NameEvent}</h1>
-                <button>
-                    <img className="BtnImg" src="https://firebasestorage.googleapis.com/v0/b/programacion-ec39e.appspot.com/o/edit.png?alt=media&token=46d5d42f-1190-4b51-9d00-38b09a271ceb" alt="" />
-                </button>
-            </div>
-            <div className="Host">
-                <p>{prop.Host}</p>
-            </div>
-            <div className="InfoSections">
-                <div className="InfoSecttion1">
-                    <p><strong>Event Type:</strong> {prop.EventType}</p>
-                    <p><strong>Dress Code:</strong> {prop.DressCode}</p>
-                    <p><strong>Date:</strong> {prop.Date}</p>
-                    <p><strong>Start Time:</strong> {prop.StartTime}</p>
-                    <p><strong>Location:</strong> {prop.Location}</p>
-                </div>
-                <div className="InfoSecttion2">
-                    <p><strong>Description:</strong> </p>
-                    <p> {prop.Description}</p>
-                </div>
-            </div>
-            <div className="FunctionBtns">
-                <FunctionBtn
-                    NextShooping={handleNextShooping}
-                    NextFound={handleNextFound}
-                    NextInvite={handleNextInvite}
-                />
-            </div>
+  if (!eventInfo) {
+    return <p>Loading event details...</p>;
+  }
+  return (
+    <div className="InfoCard">
+      <BackBtn NameEvent={eventInfo.name} />
+      <div className="ImgBanner">
+        <img src={prop.img} alt="" />
+      </div>
+      <div className="NameEvent">
+        <h1>{prop.NameEvent}</h1>
+      </div>
+      <div className="Host">
+        <p>{prop.Host}</p>
+      </div>
+      <div className="InfoSecttion1">
+      <div id="row1">
+        <div className="EventDescription">
+         <img src="https://raw.githubusercontent.com/valen-web/LAB5/refs/heads/main/Group%201000004562d.png" alt="EventTypeIcon" />
+          <p>{prop.EventType}</p>
+          </div>
+        <div className="EventDescription">
+          <img src="https://raw.githubusercontent.com/valen-web/LAB5/refs/heads/main/Group%201000004562.png" alt="DressCodeIcon" />
+          <p>{prop.DressCode}</p>
         </div>
-    );
+        </div>
+        <div id="row2">
+        <div className="EventDescription">
+         <img src="https://raw.githubusercontent.com/valen-web/LAB5/refs/heads/main/Group%201000004561.png" alt="DateIcon" />
+          <p>{prop.Date}</p>
+        </div>
+        <div className="EventDescription">
+         <img src="https://raw.githubusercontent.com/valen-web/LAB5/refs/heads/main/Group%20100000456e.png" alt="StartTimeIcon" />
+          <p>{prop.StartTime}</p>
+        </div>
+        </div>
+        <div id="row3">
+        <div className="EventDescription">
+          <img src="https://raw.githubusercontent.com/valen-web/LAB5/refs/heads/main/dcc.png" alt="LocationIcon" />
+          <p>{prop.Location}</p>
+        </div>
+        <div className="EditEventButton">
+          <p>{prop.Location}</p>
+        </div>
+        </div>
+      </div>
+      <div className="InfoSecttion2">
+        <p>
+          <strong>Description:</strong>{" "}
+        </p>
+        <p>{prop.Description}</p>
+        <div className="FunctionBtns">
+          <FunctionBtn
+            NextShooping={handleNextShooping}
+            NextFound={handleNextFound}
+            NextInvite={handleNextInvite}
+          />
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <ModalInvite
+          users={filteredUsers}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+          closeModal={closeModal}
+          selectedUsers={selectedUsers}
+          setSelectedUsers={setSelectedUsers}
+          handleInviteUser={handleInviteUser}
+        />
+      )}
+    </div>
+  );
 };
 
 export default InfoCard;
